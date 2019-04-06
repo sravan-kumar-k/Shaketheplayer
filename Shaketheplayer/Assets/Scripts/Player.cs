802c43a4091bc5279cd03ad2cfc46a7a55c3cc77
K SRAVAN KUMAR <sravan.kumar1691@gmail.com>
2019-04-06 20:27:27 +0530

    trying Touch Controls

--- Shaketheplayer/Assets/Scripts/Player.cs
+++ Shaketheplayer/Assets/Scripts/Player.cs
@@ -25,18 +25,29 @@ public class Player : MonoBehaviour
             Time.timeScale = 0f;
             //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
+
+        MoveRight();
+        MoveLeft();
+        transform.position = Vector2.MoveTowards(transform.position, finalPos, speed * Time.deltaTime);
+
+    }
+
+    public void MoveRight()
+    {
         if (Input.GetKeyDown(KeyCode.D)&& transform.position.x<4)
         {
             finalPos = new Vector2(transform.position.x+ xIncrement, transform.position.y);
             camAnim.SetTrigger("Shake");
         }
-        else if(Input.GetKeyDown(KeyCode.A)&&transform.position.x>-4)
-        {
+
+    }
+
+    public void MoveLeft()
+    {
+       if(Input.GetKeyDown(KeyCode.A)&&transform.position.x>-4)
+       {
             finalPos = new Vector2(transform.position.x - xIncrement, transform.position.y);
             camAnim.SetTrigger("Shake");
-        }
-        
-        transform.position = Vector2.MoveTowards(transform.position, finalPos, speed * Time.deltaTime);
-
+      }
     }
 }
---
+++ Shaketheplayer/Assets/UserInterface.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class UserInterface : MonoBehaviour
+{
+    public void NewGame()
+    {
+        SceneManager.LoadScene("Main");
+    }
+}
