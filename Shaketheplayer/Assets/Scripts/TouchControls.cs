fc5a2f02a79597db879294f37942d5829325b1ed
K SRAVAN KUMAR <sravan.kumar1691@gmail.com>
2019-04-07 18:46:25 +0530

    touch inputs on mobile

---
+++ Shaketheplayer/Assets/Scripts/TouchControls.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchControls : MonoBehaviour
+{
+    private Player player;
+
+    void Start()
+    {
+        player = GetComponent<Player>();
+    }
+
+    void Update()
+    {
+        if (Input.touchCount>0)
+        {
+            Touch touch = Input.GetTouch(0);
+            Vector3 touch_Pos = Camera.main.ScreenToWorldPoint(touch.position);
+            if(touch_Pos.x>0)
+            {
+                player.MoveRight();
+            } if(touch_Pos.x<0)
+            {
+                player.MoveLeft();
+            }
+
+        }
+    }
+}
